e8b985e34da47c6b179d08a2a4b3817e47cd76f8
Pedro <pedropereirarodrigues16@gmail.com>
2025-10-02 07:25:04 -0300

    Update paisesroutes.ts
    atualizado "create" dos países

--- src/server/routes/paisesroutes.ts
+++ src/server/routes/paisesroutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', paisesController.getAll);
 router.get('/:id', paisesController.getById);
-router.get('/', paisesController.create);
+router.post('/', paisesController.create);
 router.put('/:id', paisesController.update);
 router.delete('/:id', paisesController.remove);
 
